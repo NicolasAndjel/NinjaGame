20c3816ac6690e7b9a0cc37554666df231f07738
Nicolás Ignacio Andjel <andjelnicolas@hotmail.com>
2018-11-08 22:51:25 -0300

    Created prefabs for (I think) all reusable objects from scene to scene. Created levelFirst background and basic physics transferred from level1. Have to stop the flickering every time the character makes an action. Fixed new enemies being pushed by character. Fixed camera not showing objects after falling (z axis solved it). Still missing differenciating feature for heavy samurai.

---
+++ Ninja Game/Assets/Scripts/Triggers/FightTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FightTrigger : MonoBehaviour {
+
+    GameManager gameManager;
+
+	// Use this for initialization
+	void Start () {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.gameObject.layer == 0)
+    //    {
+    //        print("El trigger sabe que el ninja pasó");
+    //        gameManager.TrapActivated();
+    //    }
+    //}
+}
